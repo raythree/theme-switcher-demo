d1295b28bebcc68ab0677d7c2cb751b1078eda0f
Bill Walker <bill@Bills-MacBook-Pro-2.local>
2016-08-06 14:28:09 -0400

    added tables

--- src/components/PageOne.js
+++ src/components/PageOne.js
@@ -80,6 +80,63 @@ class PageOne extends React.Component {
       </fieldset>
     </form>
 
+    let table = (
+      <table className="table table-striped table-hover ">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Column heading</th>
+            <th>Column heading</th>
+            <th>Column heading</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Column content</td>
+            <td>Column content</td>
+            <td>Column content</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Column content</td>
+            <td>Column content</td>
+            <td>Column content</td>
+          </tr>
+          <tr className="info">
+            <td>3</td>
+            <td>Column content</td>
+            <td>Column content</td>
+            <td>Column content</td>
+          </tr>
+          <tr className="success">
+            <td>4</td>
+            <td>Column content</td>
+            <td>Column content</td>
+            <td>Column content</td>
+          </tr>
+          <tr className="danger">
+            <td>5</td>
+            <td>Column content</td>
+            <td>Column content</td>
+            <td>Column content</td>
+          </tr>
+          <tr className="warning">
+            <td>6</td>
+            <td>Column content</td>
+            <td>Column content</td>
+            <td>Column content</td>
+          </tr>
+          <tr className="active">
+            <td>7</td>
+            <td>Column content</td>
+            <td>Column content</td>
+            <td>Column content</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+
     let alerts =
     <div>
       <div className="alert alert-dismissible alert-success">
@@ -270,6 +327,13 @@ class PageOne extends React.Component {
           <div className="col-md-6">
             <h3>Alerts</h3>
             {alerts}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <h3>Tables</h3>
+            {table}
           </div>
         </div>
       </div>
